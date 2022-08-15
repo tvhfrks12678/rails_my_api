feef9e79fdea12969a90fa21635329d64ba8285c
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-08-15 17:20:57 +0900

    feat: add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: :index
+  
+  namespace 'api' do
+    namespace 'v1' do
+      resources :posts
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
